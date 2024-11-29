0311b7daa978568be35aa2bde58f062c58fa5da3
soparodi <soparodi@outlook.it>
2024-11-29 12:00:35 +0100

    01_metodi-console

--- xx-Esercitazioni/01_metodi-console/Program.cs
+++ xx-Esercitazioni/01_metodi-console/Program.cs
@@ -1,48 +1,60 @@
-﻿// questo è un commento a riga singola
+﻿// questo e un commento a riga singola
 
 /*
-questo è un commento a riga multipla
+questo e un commento a riga multipla
+*/
+
+/*
+i metodi di console permettono di gestire l output o l input (il dialogo con l utente) attraverso la console
+WriteLine() stampa a video una stringa
+ReadLine() legge una stringa da tastiera
+ReadKey() legge un tasto premuto da tastiera
+
 */
 
 // il metodo Console.Writeline stampa a video il testo passato come argomento
-Console.WriteLine("Ciao!"); // questo è un commento a fine riga
+Console.WriteLine("Hello World!");
 
-// il metodo Console.Readline legge una riga di testo da tastiera
-Console.WriteLine("Inserisci il tuo nome");
-string nome = Console.ReadLine(); // legge una riga di testo e la assegna alla variabile nome
+// il metodo Console.ReadLine legge una riga di testo da tastiera
+string nome = Console.ReadLine(); // legge una riga di testo da tastiera e la assegna alla variabile nome
+
 // stampo il nome concatenato con una stringa
 Console.WriteLine("Ciao " + nome + "!"); // con il segno + posso concatenare stringhe con variabili
-// stampo il nome concatenato con una stringa utilizzando l'interpolazione
-Console.WriteLine("Inserisci il tuo cognome");
+
+// stampo il nome concatenato con una stringa utilizzando l interpolazione
+Console.WriteLine($"Ciao {nome}!"); // con l'interpolazione posso concatenare stringhe con variabili
+
+// chiedo all utente di inserire il proprio nome
+Console.WriteLine("Inserisci il tuo cognome:");
+// leggo il cognome da tastiera e lo assegno alla variabile cognome
 string cognome = Console.ReadLine();
-Console.WriteLine($"Ciao {nome} {cognome}!"); // con l'interpolazione posso concatenare più variabili in modo semplificato
-Console.WriteLine($"Ciao {nome.ToUpper()}!"); // con il metodo To Upper() trasforma una riga in maiuscolo
-Console.WriteLine($"Ciao {nome.ToLower()}!"); // con il metodo To Lower() trasforma una riga in minuscolo
-// chiedo all'utente di inserire l'età
-Console.WriteLine("Inserisci la tua età");
-// leggo l'età da tastiera e la assegno alla variabile età
-string età = Console.ReadLine();
-// stampo l'età
-Console.WriteLine($"Hai {età} anni.");
-//stampo nome cognome ed età concatenati
-Console.WriteLine($"Ciao {nome} {cognome}! Hai {età} anni.");
-// dichiaro una variabile intera etàInt
-int etàInt = 35;
-string etàStr = etàInt.ToString();
-// concateno etàInt con una stringa
-Console.WriteLine($"Hai {etàInt} anni.");
+// stampo il cognome
+Console.WriteLine($"Ciao {nome} {cognome}!"); // con l'interpolazione posso concatenare piu variabili in modo semplificato
 
-/*
-i metodi di console permettono di gestire l'output o l'input (il dialogo con l'utente) attraverso la console
-WriteLine() stampa a video una stringa
-ReadLine() legge una stringa da tastiera
+// dichiaro una variabile intera etaInt
+int etaInt = 47; // inizializzo la variabile etaInt con il valore 47
 
-ho utilizzato due variabili di tipo string per memorizzare il nome e cognome dell'utente
-ho utilizzato una variabile di tipo string per memorizzare l'età dell'utente
+// dichiaro una variabile stringa etaStr
+string etaStr = etaInt.ToString();
 
-il metodo ToUpper() trasforma una riga in maiuscolo
-il metodo ToLower() trasforma una riga in minuscolo
+// concateno etaInt con una stringa
+Console.WriteLine($"Hai {etaInt} anni");
 
-ho provato ad utilizzare la variabile intera direttamente ma mi ha dato errore perché doveva esserea convertita in stringa
-ho utilzzato il metodo To String() per convertire un intero in una stringa e l'ho interpolata
-*/
+// leggo l input dell utente con ReadKey();
+Console.ReadKey(); // legge un tasto premuto da tastiera
+
+// se il tasto premuto e uguale a Enter
+if (Console.ReadKey().Key == ConsoleKey.Enter)
+{
+    // stampo un messaggio
+    Console.WriteLine("Hai premuto il tasto Enter");
+}
+
+// se il tasto premuto e uguale a s esco dal programma
+if (Console.ReadKey().Key == ConsoleKey.S)
+{
+    // stampo un messaggio
+    Console.WriteLine("Hai premuto il tasto S");
+    // esco dal programma
+    return;
+}
