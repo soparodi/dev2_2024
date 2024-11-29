36a44e1a6448def4df298a1c60d9a1794e47c4c0
soparodi <soparodi@outlook.it>
2024-11-29 12:52:20 +0100

    15_metodi-file

--- xx-Esercitazioni/15_metodi-file/Program.cs
+++ xx-Esercitazioni/15_metodi-file/Program.cs
@@ -1,22 +1,23 @@
-﻿// I metodi di files
+﻿// I Metodi di files
 
 // Creare un file
 string path = @"test.txt";
 File.Create(path).Close();
 
 // Scrivere su un file
-File.WriteAllText(path, "Hello World!")
+File.WriteAllText(path, "Hello, World!");
 
 // Leggere da un file
 string content = File.ReadAllText(path);
+// stampo il contenuto del file
 Console.WriteLine(content);
 
 // Copiare un file
-string path2 = @"test.txt";
+string path2 = @"test2.txt";
 File.Copy(path, path2);
 
 // Rinominare un file
-string path3 = @"test.txt";
+string path3 = @"test3.txt";
 File.Move(path2, path3);
 
 // Eliminare un file
@@ -34,6 +35,6 @@ string tempFile = Path.GetTempFileName();
 Console.WriteLine(tempFile);
 
 // Creare un file temporaneo in una directory specifica
-// Path.Combine unisce i path, in questo caso aggiunge "temp" alla directory temporanea
+// Path.Combine unisce i path in questo caso aggiunge "temp" alla directory temporanea
 string tempDir = Path.Combine(Path.GetTempPath(), "temp");
 Directory.CreateDirectory(tempDir);
