5939d49f86e1cda55d0371a060b735c24c62d7dd
soparodi <soparodi@outlook.it>
2024-11-29 12:50:09 +0100

    14_persistenza-txt

--- xx-Esercitazioni/14_persistenza-txt/Program.cs
+++ xx-Esercitazioni/14_persistenza-txt/Program.cs
@@ -1,48 +1,81 @@
-﻿string path = @"test.txt"; // in questo caso il file è nella stessa cartella del programma
-string[] lines = File.ReadAllLines(path); // legge tutte le righe del programma e le mette in un array di stringhe
+﻿// GESTIONE FILES TXT
 
-bool nessunNomeCon = true;
+// LEGGERE UN CONTENUTO DA UN FILE TXT
+
+string path = @"test.txt"; // in questo caso il file è nella stessa cartella del programma
+string[] lines = File.ReadAllLines(path); // legge tutte le righe del file e le mette in un array di stringhe
 
 foreach (string line in lines)
 {
-    if (line.StartsWith("S")) // controlla se la riga inizia con una determinata lettera
+    Console.WriteLine(line); // stampa la riga
+}
+
+// LEGGERE UN CONTENUTO DA UN TXT E STAMPARE SOLO LE RIGHE CHE INIZIANO CON UNA DETERMINATA LETTERA
+
+// string path = @"test.txt";
+// string[] lines = File.ReadAllLines(path);
+
+foreach (string line in lines)
+{
+    if (line.StartsWith("a")) // controlla se la riga inizia con la lettera
     {
         Console.WriteLine(line); // stampa la riga
-        nessunNomeCon = false;
     }
 }
 
-if (nessunNomeCon)
-{
-    Console.WriteLine("NOT FOUND");
-}
+// LEGGERE UN CONTENUTO DA UN TXT E STAMPARE SOLO LE RIGHE CHE INIZIANO CON UNA DETERMINATA LETTERA
+// SE NESSUN NOME INCOMINCIA CON LA LETTERA SCELTA DA UN MESSAGGIO DI ERRORE
 
-string path2 = @"testOutput.txt";
-File.CreateText(path2).Close();
+bool nessunNomeIniziaConLettera = true; // dichiaro un booleano che inizialmente è vero
 
 foreach (string line in lines)
 {
-    if (line.StartsWith("S")) // controlla se la riga inizia con una determinata lettera
+    if (line.StartsWith("a"))
+    {
+        Console.WriteLine(line);
+        nessunNomeIniziaConLettera = false; // se trovo un nome che inizia con la lettera, il booleano diventa falso
+    }
+}
+
+if (nessunNomeIniziaConLettera) // se il booleano è rimasto vero, nessun nome inizia con la lettera
+{
+    Console.WriteLine("Nessun nome inizia con la lettera scelta");
+}
+
+// LEGGERE UN CONTENUTO DA UN TXT E STAMPARE SOLO LE RIGHE CHE INIZIANO CON UNA DETERMINATA LETTERA
+// SE NESSUN NOME INCOMINCIA CON LA LETTERA SCELTA DA UN MESSAGGIO DI ERRORE
+// CREARE UN TXT CON LE RIGHE CHE INIZIANO CON LA LETTERA SCELTA
+
+string path2 = @"testOutput.txt"; // in questo caso il file è nella stessa cartella del programma
+File.Create(path2).Close(); // crea il file e lo chiude subito
+
+foreach (string line in lines)
+{
+    if (line.StartsWith("a"))
     {
         // Console.WriteLine(line);
-        nessunNomeCon = false; // se trovo un nome che inizia con la lettera, il booleano diventa falso
+        nessunNomeIniziaConLettera = false; // se trovo un nome che inizia con la lettera, il booleano diventa falso
         File.AppendAllText(path2, line + "\n"); // scrive la riga nel file di output andando a capo
     }
 }
-
-if (nessunNomeCon)
+if (nessunNomeIniziaConLettera) // se il booleano è vero allora stampa "nessun nome inizia con la lettera a"
 {
-    Console.WriteLine("NOT FOUND");
+    Console.WriteLine("nessun nome inizia con la lettera scelta");
 }
 
 // AGGIUNGERE UNA RIGA DI TESTO IN UNA POSIZIONE SPECIFICA
 
-// stampo la lunghezza dell'array
+// stampo la lunghezza dell array
 Console.WriteLine(lines.Length);
-lines[lines.Length - 1] += "INDIRIZZO"; // aggiunge indirizzo alla penultima riga
+lines[lines.Length - 2] += "indirizzo";
 File.WriteAllLines(path2, lines); // scrive tutte le righe nel file
 
-// AGGIUNGERE UNA RIGA IN UNA POSIZIONE SPECIFICA USANDO ACCENTO CIRCONFLESSO
+// AGGIUNGERE UNA RIGA DI TESTO IN UNA POSIZIONE SPECIFICA USANDO L ACCENTO CIRCONFLESSO
 
-lines[^1] = "\n TELEFONO"; // aggiunge indirizzo alla penultima riga
+lines[^ 2] += "numero di telefono 2"; // aggiunge il testo partendo dall ultima posizione
+File.WriteAllLines(path2, lines); // scrive tutte le righe nel file
+
+// SOVRASCRIVERE UNA RIGA DI TESTO IN UNA POSIZIONE SPECIFICA
+
+lines[lines.Length - 2] = "numero di telefono"; // aggiunge "NUMERO DI TELEFONO" alla penultima riga
 File.WriteAllLines(path2, lines); // scrive tutte le righe nel file
